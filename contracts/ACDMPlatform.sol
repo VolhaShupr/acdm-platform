e9fa5ef8f16bf8ecb48a181bd7e65a5e67808d95
Volha Shupranava <volha_shupr@tut.by>
2022-05-04 16:33:57 +0400

    Add referral reward

--- contracts/ACDMPlatform.sol
+++ contracts/ACDMPlatform.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./Token.sol";
 
@@ -11,14 +11,19 @@ import "./Token.sol";
 
 error InappropriateRound();
 
-contract ACDMPlatform is Ownable, ReentrancyGuard {
+contract ACDMPlatform is AccessControl, ReentrancyGuard {
 
     using SafeERC20 for Token;
 
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant DAO_ROLE = keccak256("DAO_ROLE");
+
     Token public immutable token;
-    uint tokenDecimalsMultiplier;
+    uint private immutable _tokenDecimalsMultiplier;
 
     uint public roundDuration;
+
+    address public referralRewardHolder;
 
     enum RoundType { Sale, Trade }
 
@@ -42,12 +47,26 @@ contract ACDMPlatform is Ownable, ReentrancyGuard {
 
     mapping (uint => Order) private _orders;
 
+    // tried to pack into 2 uint256 variables, but the size increased
+    struct ReferralReward {
+        uint saleRoundL1;
+        uint saleRoundL2;
+        uint tradeRoundL1;
+        uint tradeRoundL2;
+    }
+
+    ReferralReward refReward;
+
+    mapping (address => address) public referrers;
+
+    event UserRegistered(address indexed referee, address indexed referrer);
     event RoundStarted(RoundType indexed roundType, uint tokenPrice, uint tokenAmount);
     event RoundStarted(RoundType indexed roundType);
     event SaleTokenBought(address indexed buyer, uint tokenAmount);
     event OrderAdded(uint indexed id, address indexed owner, uint amount, uint price);
     event OrderRemoved(uint indexed id, uint retrnedAmount);
     event OrderRedeemed(uint indexed id, address indexed buyer, uint amount, uint price);
+    event RefRewardConfigUpdated(RoundType indexed roundType, uint newLevel1Value, uint newLevel2Value);
 
     modifier isRound(RoundType roundType) {
         if (currentRound.roundType == roundType && currentRound.endDate > block.timestamp) {
@@ -57,14 +76,34 @@ contract ACDMPlatform is Ownable, ReentrancyGuard {
         }
     }
 
-    constructor(address _token, uint _roundDuration) {
+    constructor(address _token, uint _roundDuration, address _referralRewardHolder) {
         token = Token(_token);
-        tokenDecimalsMultiplier = 10 ** token.decimals();
+        _tokenDecimalsMultiplier = 10 ** token.decimals();
         roundDuration = _roundDuration;
 
         currentRound.roundType = RoundType.Trade;
-        currentRound.salePriceInEth = 0.00001 ether; // todo move to constructor parameters
+        currentRound.salePriceInEth = 0.00001 ether; // can be moved to constructor parameters
         currentRound.tradeEthVolume = 1 ether;
+
+        refReward.saleRoundL1 = 500; // 5%
+        refReward.saleRoundL2 = 300; // 3%
+        refReward.tradeRoundL1 = 250; // 2.5%
+        refReward.tradeRoundL2 = 250; // 2.5%
+
+        referrers[msg.sender] = msg.sender; // register first referrer
+
+        referralRewardHolder = _referralRewardHolder;
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function register(address referrer) external {
+        require(referrer != address(0), "Not valid referrer address");
+        require(referrer != msg.sender, "Not valid referrer address");
+        require(referrers[referrer] != address(0), "Referrer should be registered");
+        require(referrers[msg.sender] == address(0), "Reference already exists");
+
+        referrers[msg.sender] = referrer;
+        emit UserRegistered(msg.sender, referrer);
     }
 
     function startSaleRound() external {
@@ -72,17 +111,15 @@ contract ACDMPlatform is Ownable, ReentrancyGuard {
             revert InappropriateRound();
         }
 
+        // takes initial token price in the first round; uses formula: lastPrice*1,03+0,000004 in the next rounds
         uint lastPrice = currentRound.salePriceInEth;
-
-        // takes initial token price in the first round
-        // uses formula: lastPrice*1,03+0,000004 in the next rounds
         uint newTokenPrice = (currentRound.endDate > 0) ? (lastPrice * 103) / 100 + 0.000004 ether : lastPrice;
-        currentRound.salePriceInEth = newTokenPrice;
+        uint newTokenAmount = _calcTokenAmount(currentRound.tradeEthVolume, newTokenPrice);
+
         currentRound.endDate = block.timestamp + roundDuration;
         currentRound.roundType = RoundType.Sale;
-
-        uint newTokenAmount = _calcTokenAmount(currentRound.tradeEthVolume, currentRound.salePriceInEth);
         currentRound.saleTokensLeft = newTokenAmount;
+        currentRound.salePriceInEth = newTokenPrice;
         currentRound.tradeEthVolume = 0;
 
         token.mint(address(this), newTokenAmount);
@@ -91,6 +128,8 @@ contract ACDMPlatform is Ownable, ReentrancyGuard {
     }
 
     function buySaleTokens() external payable isRound(RoundType.Sale) nonReentrant {
+        require(currentRound.saleTokensLeft > 0, "No tokens left");
+
         uint amountToBuy = _calcTokenAmount(msg.value, currentRound.salePriceInEth);
         require(amountToBuy > 0, "Not enough ether to buy a token");
 
@@ -102,17 +141,23 @@ contract ACDMPlatform is Ownable, ReentrancyGuard {
         currentRound.saleTokensLeft -= amountToBuy;
         token.safeTransfer(msg.sender, amountToBuy);
 
-        uint spentEth = amountToBuy * currentRound.salePriceInEth / tokenDecimalsMultiplier;
+        uint spentEth = amountToBuy * currentRound.salePriceInEth / _tokenDecimalsMultiplier;
         if (msg.value > spentEth) {
             _transferEth(msg.sender, msg.value - spentEth); // gives a change
         }
 
+        // referral program
+        address referrerL1 = referrers[msg.sender];
+        if (referrerL1 != address(0)) {
+            uint rewardL1 = spentEth * refReward.saleRoundL1 / 10000;
+            _transferEth(referrerL1, rewardL1);
+
+            uint rewardL2 = spentEth * refReward.saleRoundL2 / 10000;
+            _transferEth(referrers[referrerL1], rewardL2);
+        }
+
         emit SaleTokenBought(msg.sender, amountToBuy);
     }
-
-//    function getAvailableTokensAmountForSale() external returns (uint) {
-//        return currentRound.endDate > block.timestamp ? currentRound.remainingTokens : 0;
-//    }
 
     function startTradeRound() external {
         if (currentRound.roundType == RoundType.Trade ||
@@ -162,29 +207,40 @@ contract ACDMPlatform is Ownable, ReentrancyGuard {
 
     function redeemOrder(uint id) external payable isRound(RoundType.Trade) nonReentrant {
         Order storage order = _orders[id];
-        require(order.price > 0, "Invalid order id");
+        require(order.remainingAmount > 0, "Order doesn't exist or completed");
 
         uint amount = _calcTokenAmount(msg.value, order.price);
         require(amount > 0, "Not enough ether to buy a token");
-        // todo return to the buyer exceeded amount
-        require(order.remainingAmount >= amount, "Not enough tokens left");
 
+        if (amount > order.remainingAmount) {
+            amount = order.remainingAmount; // can buy only available tokens
+        }
+
+        // todo unchecked
         order.remainingAmount -= amount;
 
         token.safeTransfer(msg.sender, amount);
-        _transferEth(order.owner, msg.value);
+        uint spentEth = amount * order.price / _tokenDecimalsMultiplier;
+        uint rewardL1 = spentEth * refReward.tradeRoundL1 / 10000;
+        uint rewardL2 = spentEth * refReward.tradeRoundL2 / 10000;
 
-        currentRound.tradeEthVolume += msg.value;
+        _transferEth(order.owner, spentEth - rewardL1 - rewardL2);
+        if (msg.value > spentEth) {
+            _transferEth(msg.sender, msg.value - spentEth); // gives a change
+        }
 
-        // todo add partially filled or filled?
+        // referral program
+        address referrerL1 = referrers[order.owner];
+        if (referrerL1 != address(0)) {
+            _transferEth(referrerL1, rewardL1);
+            _transferEth(referrers[referrerL1], rewardL2);
+        } else {
+            _transferEth(referralRewardHolder, rewardL1 + rewardL2);
+        }
+
+        currentRound.tradeEthVolume += spentEth;
+
         emit OrderRedeemed(id, msg.sender, amount, order.price);
-    }
-
-    function withdrawEth(address to, uint amount) external onlyOwner {
-        require(to != address(0), "Not valid recipient address");
-        require(amount > 0 && amount <= address(this).balance, "Insufficient ether amount to transfer");
-
-        _transferEth(to, amount);
     }
 
     function _transferEth(address to, uint amount) private {
@@ -193,7 +249,33 @@ contract ACDMPlatform is Ownable, ReentrancyGuard {
     }
 
     function _calcTokenAmount(uint volumeInEth, uint priceInEth) private view returns(uint) {
-        return volumeInEth * tokenDecimalsMultiplier / priceInEth;
+        return volumeInEth * _tokenDecimalsMultiplier / priceInEth;
     }
 
+    // --- ADMIN functions ---
+    function withdrawEth(address to, uint amount) external onlyRole(ADMIN_ROLE) {
+        require(to != address(0), "Not valid recipient address");
+        require(amount > 0 && amount <= address(this).balance, "Insufficient ether amount to transfer");
+
+        _transferEth(to, amount);
+    }
+
+    function updateRefRewards(RoundType round, uint level1, uint level2) external onlyRole(DAO_ROLE) {
+        if (round == RoundType.Sale) {
+            refReward.saleRoundL1 = level1;
+            refReward.saleRoundL2 = level2;
+        } else {
+            refReward.tradeRoundL1 = level1;
+            refReward.tradeRoundL2 = level2;
+        }
+        emit RefRewardConfigUpdated(round, level1, level2);
+    }
+
+    function updateRoundDuration(uint newRoundDuration) external onlyRole(ADMIN_ROLE) {
+        roundDuration = newRoundDuration;
+    }
+
+    function updateReferralRewardHolder(address newReferralRewardHolder) external onlyRole(ADMIN_ROLE) {
+        referralRewardHolder = newReferralRewardHolder;
+    }
 }
--- test/acdm-platform.spec.ts
+++ test/acdm-platform.spec.ts
@@ -11,11 +11,19 @@ const toAcdmBigNumber = (amount: number): BigNumber => toBigNumber(amount, ACDM_
 const calcPrice = (lastPrice: BigNumber): BigNumber => lastPrice.mul(103).div(100).add(toBigNumber(0.000004));
 const calcAcdmAmount = (volume: BigNumber, price: BigNumber): BigNumber => volume.mul(10 ** ACDM_DECIMALS).div(price);
 const calcAcdmCost = (amount: BigNumber, price: BigNumber): BigNumber => amount.mul(price).div(10 ** ACDM_DECIMALS);
+const calcPercent = (amount: BigNumber, percent: number, multiplier = 100): BigNumber => amount.mul(percent).div(100 * multiplier);
 
 enum RoundType {
   Sale,
   Trade
 }
+
+const Reward = {
+  saleL1: 500,
+  saleL2: 300,
+  tradeL1: 250,
+  tradeL2: 250,
+};
 
 describe("ACDM Platform", () => {
 
@@ -33,13 +41,18 @@ describe("ACDM Platform", () => {
   const orderAcdmAmount = toAcdmBigNumber(50000);
   const orderTokenPrice = toBigNumber(0.0001);
 
+  const redeemEthValue = toBigNumber(0.5); // 0.5 eth
+  const redeemAcdmAmount = calcAcdmAmount(redeemEthValue, orderTokenPrice); // 5000 acdm
+
   let acdmToken: Contract,
     platform: Contract,
     owner: SignerWithAddress,
     account1: SignerWithAddress,
     account2: SignerWithAddress,
     account3: SignerWithAddress,
+    referralRewardAccount: SignerWithAddress,
     platformAddress: string,
+    ownerAddress: string,
     account1Address: string,
     account2Address: string,
     account3Address: string;
@@ -47,7 +60,8 @@ describe("ACDM Platform", () => {
   let clean: any; // snapshot
 
   before(async () => {
-    [owner, account1, account2, account3] = await ethers.getSigners();
+    [owner, account1, account2, account3, referralRewardAccount] = await ethers.getSigners();
+    ownerAddress = owner.address;
     account1Address = account1.address;
     account2Address = account2.address;
     account3Address = account3.address;
@@ -57,7 +71,7 @@ describe("ACDM Platform", () => {
     await acdmToken.deployed();
 
     // --- ACDM platform deployment ---
-    platform = await deploy("ACDMPlatform", [acdmToken.address, roundDuration]);
+    platform = await deploy("ACDMPlatform", [acdmToken.address, roundDuration, referralRewardAccount.address]);
     platformAddress = platform.address;
     const minterRole = ethers.utils.id("MINTER_ROLE");
     await acdmToken.grantRole(minterRole, platformAddress);
@@ -66,12 +80,45 @@ describe("ACDM Platform", () => {
     await acdmToken.connect(account2).approve(platformAddress, approveAmount);
     await acdmToken.connect(account3).approve(platformAddress, approveAmount);
 
+    const adminRole = ethers.utils.id("ADMIN_ROLE");
+    await platform.grantRole(adminRole, ownerAddress);
+
     clean = await network.provider.request({ method: "evm_snapshot", params: [] });
   });
 
   afterEach(async () => {
     await network.provider.request({ method: "evm_revert", params: [clean] });
     clean = await network.provider.request({ method: "evm_snapshot", params: [] });
+  });
+
+  describe("[register]", () => {
+    it("Should revert when referrer is zero address", async () => {
+      await expect(platform.register(ZERO_ADDRESS)).to.be.revertedWith("Not valid referrer address");
+    });
+
+    it("Should revert when referrer has the same address as a sender", async () => {
+      await expect(platform.connect(account1).register(account1Address)).to.be.revertedWith("Not valid referrer address");
+    });
+
+    it("Should revert when referrer has been not registered", async () => {
+      await expect(platform.connect(account1).register(account2Address)).to.be.revertedWith("Referrer should be registered");
+    });
+
+    it("Should revert when reference already exists", async () => {
+      await platform.connect(account1).register(ownerAddress);
+      await platform.connect(account2).register(ownerAddress);
+      await expect(platform.connect(account1).register(account2Address)).to.be.revertedWith("Reference already exists");
+    });
+
+    it("Should start sale round and correct calculate token sale price and amount", async () => {
+      await expect(platform.connect(account1).register(ownerAddress))
+        .to.emit(platform, "UserRegistered")
+        .withArgs(account1Address, ownerAddress);
+      await expect(platform.connect(account2).register(account1Address))
+        .to.emit(platform, "UserRegistered")
+        .withArgs(account2Address, account1Address);
+    });
+
   });
 
   describe("[startSaleRound]", () => {
@@ -116,6 +163,11 @@ describe("ACDM Platform", () => {
       expect(await acdmToken.balanceOf(platformAddress)).to.equal(saleRound1AcdmAmount);
     });
 
+    it("Should revert when no tokens left", async () => {
+      await platform.connect(account1).buySaleTokens({ value: toBigNumber(1) });
+      await expect(platform.connect(account2).buySaleTokens({ value: ethValue1 })).to.be.revertedWith("No tokens left");
+    });
+
     it("Should buy some tokens", async () => {
       const buyTokenTx = platform.connect(account1).buySaleTokens({ value: ethValue1 });
 
@@ -130,33 +182,46 @@ describe("ACDM Platform", () => {
     });
 
     it("Should buy some tokens (less than amount left) and receive change", async () => {
+      // register referrers
+      await platform.connect(account1).register(ownerAddress);
+
       const ethValue = toBigNumber(0.000099000005);
       const acdmAmount = calcAcdmAmount(ethValue, saleRound1Price); // 9.9 acdm
       const spentEth = calcAcdmCost(acdmAmount, saleRound1Price); // 0.000099 eth; 0.000000000005 eth should be transferred back to sender
+      const reward = calcPercent(spentEth, Reward.saleL1 + Reward.saleL2); // all 5 + 3% goes to contract owner as a root referrer
       const tx = platform.connect(account1).buySaleTokens({ value: ethValue });
+
       await expect(tx)
         .to.emit(platform, "SaleTokenBought")
         .withArgs(account1Address, acdmAmount);
 
       await expect(await tx).to.changeEtherBalances(
-        [account1, platform],
-        [toBigNumber(-toNumber(spentEth)), spentEth],
+        [account1, platform, owner],
+        [toBigNumber(-toNumber(spentEth)), spentEth.sub(reward), reward],
       );
     });
 
     it("Should buy some tokens (in value of amount left) and receive change", async () => {
+      // register referrers
+      await platform.connect(account3).register(ownerAddress);
+      await platform.connect(account2).register(account3Address);
+      await platform.connect(account1).register(account2Address);
+
       const ethValue = toBigNumber(2.5);
       let acdmAmount = calcAcdmAmount(ethValue, saleRound1Price); // 200 000 acdm, but available are 100 000 acdm
       acdmAmount = saleRound1AcdmAmount;
-      const spentEth = calcAcdmCost(acdmAmount, saleRound1Price); // 1 eth eth; 1.5 eth should be transferred back to sender
+      const spentEth = calcAcdmCost(acdmAmount, saleRound1Price); // 1 eth ; 1.5 eth should be transferred back to sender
+      const l1Reward = calcPercent(spentEth, Reward.saleL1); // 0.05 eth goes to acc2
+      const l2Reward = calcPercent(spentEth, Reward.saleL2); // 0.03 eth goes to acc3
       const tx = platform.connect(account1).buySaleTokens({ value: ethValue });
+
       await expect(tx)
         .to.emit(platform, "SaleTokenBought")
         .withArgs(account1Address, acdmAmount);
 
       await expect(await tx).to.changeEtherBalances(
-        [account1, platform],
-        [toBigNumber(-toNumber(spentEth)), spentEth],
+        [account1, platform, account2, account3],
+        [toBigNumber(-toNumber(spentEth)), spentEth.sub(l1Reward).sub(l2Reward), l1Reward, l2Reward],
       );
     });
 
@@ -276,8 +341,6 @@ describe("ACDM Platform", () => {
   });
 
   describe("[redeemOrder]", () => {
-    const redeemEthValue = toBigNumber(0.5); // 0.5 eth
-    const redeemAcdmAmount = calcAcdmAmount(redeemEthValue, orderTokenPrice);
 
     beforeEach(async () => {
       await platform.startSaleRound();
@@ -299,7 +362,12 @@ describe("ACDM Platform", () => {
     });
 
     it("Should revert when order doesn't exist", async () => {
-      await expect(platform.redeemOrder(2, { value: redeemEthValue })).to.be.revertedWith("Invalid order id");
+      await expect(platform.redeemOrder(2, { value: redeemEthValue })).to.be.revertedWith("Order doesn't exist or completed");
+    });
+
+    it("Should revert when no tokens left in the order", async () => {
+      await platform.connect(account1).redeemOrder(orderId1, { value: toBigNumber(10) });
+      await expect(platform.connect(account2).redeemOrder(orderId1, { value: redeemEthValue })).to.be.revertedWith("Order doesn't exist or completed");
     });
 
     it("Should revert when provided ether value is not enough to buy a token", async () => {
@@ -307,20 +375,44 @@ describe("ACDM Platform", () => {
       await expect(platform.redeemOrder(orderId1, { value })).to.be.revertedWith("Not enough ether to buy a token");
     });
 
-    it("Should revert when provided ether value is greater than remaining tokens amount", async () => {
-      await expect(platform.redeemOrder(orderId1, { value: toBigNumber(11) })).to.be.revertedWith("Not enough tokens left");
-    });
-
     it("Should partially redeem the order", async () => {
       const redeemOrderTx = platform.connect(account2).redeemOrder(orderId1, { value: redeemEthValue });
+      const referralReward = calcPercent(redeemEthValue, Reward.tradeL1 + Reward.tradeL2);
 
       await expect(redeemOrderTx)
         .to.emit(platform, "OrderRedeemed")
         .withArgs(orderId1, account2Address, redeemAcdmAmount, orderTokenPrice);
 
-      await expect(await redeemOrderTx).to.changeEtherBalance(account1, redeemEthValue);
+      await expect(await redeemOrderTx).to.changeEtherBalances(
+        [account2, account1, referralRewardAccount], // [buyer, seller,`special account for reward`]
+        [toBigNumber(-toNumber(redeemEthValue)), redeemEthValue.sub(referralReward), referralReward],
+      );
       expect(await acdmToken.balanceOf(platformAddress)).to.equal(orderAcdmAmount.sub(redeemAcdmAmount));
       expect(await acdmToken.balanceOf(account2Address)).to.equal(redeemAcdmAmount);
+    });
+
+    it("Should fill the order and receive change", async () => {
+      // register referrers
+      await platform.connect(account3).register(ownerAddress);
+      await platform.connect(account2).register(account3Address);
+      await platform.connect(account1).register(account2Address);
+
+      const ethValue = toBigNumber(8);
+      let acdmAmount = calcAcdmAmount(ethValue, orderTokenPrice); // 80 000 acdm, but in order 50 000 acdm are available
+      acdmAmount = orderAcdmAmount; // max available tokens: 50 000 acdm
+      const spentEth = calcAcdmCost(acdmAmount, orderTokenPrice); // 5 eth ; 3 eth should be transferred back to sender
+      const l1Reward = calcPercent(spentEth, Reward.tradeL1); // 0.05 eth goes to acc2
+      const l2Reward = calcPercent(spentEth, Reward.tradeL2); // 0.03 eth goes to acc3
+      const tx = platform.connect(account2).redeemOrder(orderId1, { value: ethValue });
+
+      await expect(tx)
+        .to.emit(platform, "OrderRedeemed")
+        .withArgs(orderId1, account2Address, acdmAmount, orderTokenPrice);
+
+      await expect(await tx).to.changeEtherBalances(
+        [account2, account1, account3, platform], // 95% -> acc1; 2,5% -> acc2; 2,5% -> acc3
+        [l1Reward.sub(spentEth), spentEth.sub(l1Reward).sub(l2Reward), l2Reward, 0],
+      );
     });
 
   });
@@ -340,40 +432,78 @@ describe("ACDM Platform", () => {
       );
     });
 
+    it("[updateSaleRoundRefRewards] Should set new referral reward percent for sale and trade rounds", async () => {
+      const l1 = 300; // 3%
+      const l2 = 200; // 2%
+
+      const daoRole = ethers.utils.id("DAO_ROLE");
+      await platform.grantRole(daoRole, ownerAddress);
+
+      await expect(platform.updateRefRewards(RoundType.Sale, l1, l2))
+        .to.emit(platform, "RefRewardConfigUpdated")
+        .withArgs(RoundType.Sale, l1, l2);
+
+      await expect(platform.updateRefRewards(RoundType.Trade, l1, l2))
+        .to.emit(platform, "RefRewardConfigUpdated")
+        .withArgs(RoundType.Trade, l1, l2);
+    });
+
+    it("[updateRoundDuration] Should set a new round duration", async () => {
+      const newRoundDuration = 5 * 24 * 60 * 60; // 5 days;
+
+      await platform.updateRoundDuration(newRoundDuration);
+      expect(await platform.roundDuration()).to.equal(newRoundDuration);
+    });
+
+    it("[updateReferralRewardHolder] Should set a new referral reward address", async () => {
+      const newReferralRewardHolder = ownerAddress;
+
+      await platform.updateReferralRewardHolder(newReferralRewardHolder);
+      expect(await platform.referralRewardHolder()).to.equal(newReferralRewardHolder);
+    });
+
   });
 
-  // describe("end-to-end", () => {
-  //   it("Should test multiple rounds", async () => {
-  //     // --- [1] sale round ---
-  //     await platform.startSaleRound();
-  //     await platform.connect(account1).buySaleTokens({ value: ethValue1 });
-  //     await increaseTime(roundDuration);
-  //
-  //     // --- [1] trade round ---
-  //     await platform.startTradeRound();
-  //
-  //
-  //     //
-  //     // // trade round 1
-  //     // await platform.startTradeRound(); // burned saleRound1AcdmAmount - soldTokens = 0.3 ether
-  //     // const orderAmount = saleRound1AcdmAmount.div(2); // 500 000
-  //     // const orderTokenPrice = saleRound1Price.mul(10); // 0.0001 ether
-  //     // await platform.connect(account1).addOrder(orderAmount, orderTokenPrice);
-  //     // const redeemCost = toBigNumber(0.5);
-  //     // await platform.connect(account2).redeemOrder(orderId1, { value: redeemCost });
-  //     // const redeemAmount = calcAcdmAmount(redeemCost, orderTokenPrice);
-  //     // const remainingOrderTokensOnPlatformBalance = orderAmount.sub(redeemAmount);
-  //     // await increaseTime(roundDuration);
-  //     //
-  //     // // sale round 2
-  //     // const round2Amount = calcAcdmAmount(redeemCost, saleRound2Price);
-  //     // await expect(platform.startSaleRound())
-  //     //   .to.emit(platform, "RoundStarted(uint8,uint256,uint256)")
-  //     //   .withArgs(RoundType.Sale, saleRound2Price, round2Amount);
-  //     // expect(await acdmToken.totalSupply()).to.equal(soldTokens).add(round2Amount);
-  //     // expect(await acdmToken.balanceOf(platformAddress)).to.equal(remainingOrderTokensOnPlatformBalance.add(round2Amount));
-  //   });
-  //
-  // });
+  describe("additional", () => {
+    it("Should test token price and trade volume in multiple rounds", async () => {
+      // --- [1] sale round --- 1 eth / 0.00001 eth = 100 000 acdm
+      await platform.startSaleRound();
+      await platform.connect(account1).buySaleTokens({ value: ethValue1 }); // 70 000 acdm * 0.00001 eth
+      await platform.connect(account1).buySaleTokens({ value: toBigNumber(0.2) }); // 20 000 acdm * 0.00001 eth
+      await increaseTime(roundDuration);
+
+      // --- [1] trade round ---
+      let TR1tradeVolume = toBigNumber(0);
+      await platform.startTradeRound();
+      await platform.connect(account1).addOrder(orderAcdmAmount, orderTokenPrice); // 50 000 acdm * 0.0001 eth
+      await platform.connect(account2).redeemOrder(orderId1, { value: redeemEthValue }); // 5000 acdm * * 0.0001 eth
+      TR1tradeVolume = TR1tradeVolume.add(redeemEthValue); // 0.5 eth
+
+      await platform.connect(account3).redeemOrder(orderId1, { value: toBigNumber(5) });
+      const TR1redeemCost2 = calcAcdmCost(orderAcdmAmount.sub(redeemAcdmAmount), orderTokenPrice); // 4.5 eth
+      TR1tradeVolume = TR1tradeVolume.add(TR1redeemCost2); // 5 eth
+
+      const TR1orderAcdmAmount2 = toAcdmBigNumber(10000);
+      const TR1orderTokenPrice2 = toBigNumber(0.001);
+      await platform.connect(account1).addOrder(TR1orderAcdmAmount2, TR1orderTokenPrice2); // 10 000 acdm * 0.001 eth
+      await increaseTime(roundDuration);
+
+      // --- [2] sale round --- 5 eth / 0.0000143 eth = 349650,349650 acdm
+      const SR2Amount = calcAcdmAmount(TR1tradeVolume, saleRound2Price);
+      await expect(platform.startSaleRound())
+        .to.emit(platform, "RoundStarted(uint8,uint256,uint256)")
+        .withArgs(RoundType.Sale, saleRound2Price, SR2Amount);
+      await increaseTime(roundDuration);
+
+      // --- [2] trade round ---
+      await platform.startTradeRound();
+      await increaseTime(roundDuration);
+
+      // --- [3] sale round --- 0 eth / 0.000018729 eth = 0 acdm
+      await expect(platform.startSaleRound())
+        .to.emit(platform, "RoundStarted(uint8,uint256,uint256)")
+        .withArgs(RoundType.Sale, saleRound3Price, 0);
+    });
+  });
 
 });
